7435d06f44fc7c15589f87b8581db434ece88327
CarmiS2 <carmlina.sandoval@gmail.com>
2022-06-26 12:55:04 -0700

    add7

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -5,7 +5,7 @@ console.log("working");
 //
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -23,7 +23,7 @@ let baseMaps = {
 };
   // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
+    center: [44.0, -80.0],
     zoom: 2,
     layers: [streets]
 });
@@ -32,18 +32,23 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 // Add GeoJSON data.
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/CarmiS2/Mapping_Earthquakes/main/majorAirports.json";
+let torontoData = "https://raw.githubusercontent.com/CarmiS2/Mapping_Earthquakes/main/torontoRoutes.json";
 
 // Grabbing our GeoJSON data.
-// Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data,{
-    onEachFeature: function(feature, layer) {
-      console.log(layer);
-      layer.bindPopup("<h3>" + "Airport Code: " + feature.properties.faa +
-      "</h3><hr><p>" + feature.properties.name + "</p>");
-    }    
-  }).addTo(map);
+L.geoJSON(data, {
+  color: "#ffffa1",
+  weight : 2,
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3>" + "Airline " + feature.properties.airline +
+     "</h3><hr><p> Destination: " + feature.properties.dst + "</p>")
+  }
+})
+.addTo(map);
 });
+     // layer.bindPopup("<h3>" + "Airport Code: " + feature.properties.faa +
+     // "</h3><hr><p>" + feature.properties.name + "</p>");
+   // }    
+
